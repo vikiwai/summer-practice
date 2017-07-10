37f9704139550bb6e9f133954051dd71b3acd21d
Victoria Bunyaeva <avikiwai@gmail.com>
2017-07-11 00:58:59 +0300

    Initialize assignment 4 with assignment 3

---
+++ assignment-04.cpp
@@ -0,0 +1,55 @@
+/*
+ *  Буняева Виктория Андреевна, БПИ 162
+ *  Задание №4
+ *  IDE: CLion
+ */
+
+#include <iostream>
+using namespace std;
+
+void print_array(int* arr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+}
+
+int main() {
+    int N;
+
+    cout << "Введите размер массива:" << endl;
+    cout << "N = ";
+    cin >> N;
+    cout << endl;
+
+    cout << "Введите элементы массива:" << endl;
+    int arr[N];
+    for (int i = 0; i < N; i++)
+    {
+        cout << "[" << i << "] = ";
+        cin >> arr[i];
+    }
+    cout << endl;
+
+    cout << "Заданный массив:" << endl;
+    print_array(arr, N);
+    cout << endl;
+
+    for(int i = N; i > 0; i--) {
+        for(int j = 0; j < i - 1; j++) {
+            if(arr[j] >= arr[j + 1]) { // Сравнивание двух соседних переменных
+                int temp = arr[j];
+                arr[j] = arr[j + 1]; // Обмен двух переменных местами
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    cout  << "Массив после сортировки:" << endl;
+    print_array(arr, N);
+
+    return 0;
+}
