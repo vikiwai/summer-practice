1969794923b0af3e8628aa080a43a35d91166dd8
Victoria Bunyaeva <avikiwai@gmail.com>
2017-07-11 02:14:36 +0300

    Count and display numbers of loop iterations and swaps

--- assignment-04.cpp
+++ assignment-04.cpp
@@ -20,27 +20,41 @@ void print_array(int* arr, int size)
 
 void bubble_sort(int* arr, int size)
 {
+    int outer = 0, inner = 0, swaps = 0;
+
     for(int i = size; i > 0; i--) {
+        outer +=1;
         for(int j = 0; j < i - 1; j++) {
+            inner +=1;
             if(arr[j] >= arr[j + 1]) { // Сравнивание двух соседних переменных
                 int temp = arr[j];
                 arr[j] = arr[j + 1]; // Обмен двух переменных местами
                 arr[j + 1] = temp;
+                swaps +=1;
             }
         }
     }
+
+    cout << "Количество выполненных внешних циклов: " << outer << endl;
+    cout << "Количество выполненных внутренних циклов: " << inner << endl;
+    cout << "Количество обменов: " << swaps << endl;
 }
 
 void buble_sort_Iverson1(int* arr, int size)
 {
+    int outer = 0, inner = 0, swaps = 0;
+
     bool already_sorted = true;
 
     for(int i = size; i > 0; i--) {
+        outer +=1;
         for(int j = 0; j < i - 1; j++) {
+            inner +=1;
             if(arr[j] >= arr[j + 1]) { // Сравнивание двух соседних переменных
                 int temp = arr[j];
                 arr[j] = arr[j + 1]; // Обмен двух переменных местами
                 arr[j + 1] = temp;
+                swaps +=1;
 
                 already_sorted = false;
             }
@@ -48,20 +62,29 @@ void buble_sort_Iverson1(int* arr, int size)
 
         if(already_sorted) break;
     }
+
+    cout << "Количество выполненных внешних циклов: " << outer << endl;
+    cout << "Количество выполненных внутренних циклов: " << inner << endl;
+    cout << "Количество обменов: " << swaps << endl;
 }
 
 void buble_sort_Iverson2(int* arr, int size)
 {
+    int outer = 0, inner = 0, swaps = 0;
+
     int last_swap = size - 1;
 
     do {
+        outer +=1;
         int new_last_swap = 0;
 
         for(int j = 0; j < last_swap; j++) {
+            inner +=1;
             if(arr[j] >= arr[j + 1]) { // Сравнивание двух соседних переменных
                 int temp = arr[j];
                 arr[j] = arr[j + 1]; // Обмен двух переменных местами
                 arr[j + 1] = temp;
+                swaps +=1;
 
                 new_last_swap = j;
             }
@@ -70,6 +93,10 @@ void buble_sort_Iverson2(int* arr, int size)
         last_swap = new_last_swap;
 
     } while (last_swap !=0);
+
+    cout << "Количество выполненных внешних циклов: " << outer << endl;
+    cout << "Количество выполненных внутренних циклов: " << inner << endl;
+    cout << "Количество обменов: " << swaps << endl;
 }
 
 void copy(int* target, int* source, int size)
