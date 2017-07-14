751b40d71bb37186e654ad0716e9381454046765
Victoria Bunyaeva <avikiwai@gmail.com>
2017-07-14 14:36:33 +0300

    Add comments and rename some variables

--- big_project.cpp
+++ big_project.cpp
@@ -4,15 +4,19 @@
  *  Среда разработки: CLion 2017.1.3
  *  Цель работы: экспериментальное определение времени сортировок различных массивов по возрастанию разными методами
  *               и сравнительный анализ полученных результатов
- *  Что сделано:
- *  Что не сделано:
+ *  Что сделано: + Реализация сортировок: пузырьком, пузырьком с I условием Айверсона, пузырьком с II условием Айверсона,
+ *                 вставками, бинарными вставками, подсчетом
+ *               + Автоматическая генерация тестов
+ *               + Автоматическое тестирование сортировок
+ *               + Вывод информации в файл с расширением .csv
+ *
+ *  Что не сделано: - Реализация цифровой сортировки
  */
 
 #include <iostream>
 #include <ctime>
-#include <fstream>
-#include <cstdlib>
 #include <chrono>
+#include <fstream>
 
 int* miniRandomArray(int size);
 int* randomArray(int size);
@@ -25,8 +29,14 @@ void swap(int& element1, int& element2);
 
 int binarySearch(int *arr, int begin, int end, int target);
 
+/**
+ * Type of sort function
+ */
 typedef void (*sortFunction)(int* arr, int size);
 
+/**
+ * Type of array
+ */
 typedef int* (*typeArray)(int size);
 
 //*********************************************************//
@@ -44,9 +54,9 @@ void countingSort(int *arr, int size);
 
 int main()
 {
-    srand((unsigned int) time(NULL));
+    srand((unsigned int) time(NULL)); // Initializing the random number generator
 
-    std::ofstream output("output.csv");
+    std::ofstream output("output.csv"); // Create a CSV file with measurement results
 
     std::string names[6] = { "Bubble", "Iverson 1", "Iverson 2", "Insertion", "Insertion Binary", "Counting" };
 
@@ -57,54 +67,73 @@ int main()
 
     typeArray type_of_array[4] = { miniRandomArray, randomArray, almostSortedArray, reverseArray };
 
-    for (int y = 0; y < 4; ++y) {
+    /*
+     * Loop passing through different types of arrays
+     */
+    for (int i_kind_arr = 0; i_kind_arr < 4; ++i_kind_arr) {
         output << ";";
         std::cout << "\t";
 
-        for (int x = 1000; x <= 9000; x += 1000){
-            output << x << ";";
-            std::cout << x << "\t";
+        /*
+         * Loop for displaying array sizes on the screen
+         */
+        for (int caption = 1000; caption <= 9000; caption += 1000){
+            output << caption << ";";
+            std::cout << caption << "\t";
         }
 
         output << std::endl;
         std::cout << std::endl;
 
-        for (int k = 0; k < 6; ++k) {
-            output << names[k] << ";";
-            std::cout << names[k] << "\t";
+        /*
+         * Loop passing through different types of sort function
+         */
+        for (int i_sort_f = 0; i_sort_f < 6; ++i_sort_f) {
+            output << names[i_sort_f] << ";";
+            std::cout << names[i_sort_f] << "\t";
 
-            for (int j = 1000; j <= 9000; j += 1000) {
+            /*
+             * Loop passing through different sizes of arrays
+             */
+            for (int size = 1000; size <= 9000; size += 1000) {
                 long long count = 0;
 
-                for (int i = 0; i < 3; ++i) {
-                    int *array = (type_of_array[y])(j);
+                for (int i = 0; i < 100; ++i) {
+                    int *array = (type_of_array[i_kind_arr])(size); // Array generation
 
-                    auto start_time = std::chrono::high_resolution_clock::now();
+                    auto start_time = std::chrono::high_resolution_clock::now(); // The start time of the algorithm
 
-                    (sort_functions_algorithm[k])(array, j);
+                    (sort_functions_algorithm[i_sort_f])(array, size); // Function call
 
-                    auto end_time = std::chrono::high_resolution_clock::now();
+                    auto end_time = std::chrono::high_resolution_clock::now(); // Time of the end of the algorithm
 
                     count += std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time).count();
+                        // Counting time in nanoseconds
 
-                    delete[] array;
+                    delete[] array; // Clear memory
                 }
 
-                output << count / 3 << ";";
-                std::cout << count / 3 << "\t" << std::flush;
-            }
+                output << count / 100 << ";";
+                std::cout << count / 100 << "\t" << std::flush;
+            } // The end of the loop passing through different sizes of arrays
 
             output << std::endl;
             std::cout << std::endl;
-        }
+        } // The end of the loop passing through different types of sort function
 
         output << std::endl;
         std::cout << std::endl;
-    }
+
+    } // The end of the loop passing through different types of arrays
 }
 
 //*********************************************************//
 
+/**
+ * Function generating an array filled with random integer values from 0 to 7
+ * @param size The size of the array
+ * @return Generated array
+ */
 int* miniRandomArray(int size)
 {
     int* array = new int[size];
@@ -116,6 +145,11 @@ int* miniRandomArray(int size)
     return array;
 }
 
+/**
+ * Function generating an array filled with random integer values from 0 to the maximum integer
+ * @param size The size of the array
+ * @return Generated array
+ */
 int* randomArray(int size)
 {
     int* array = new int[size];
@@ -127,6 +161,12 @@ int* randomArray(int size)
     return array;
 }
 
+/**
+ * Function generating an array almost sorted in the required order by numbers
+ * (for example, swapping 5 pairs of elements of the sorted array)
+ * @param size The size of the array
+ * @return Generated array
+ */
 int* almostSortedArray(int size)
 {
     int* array = new int[size];
@@ -146,6 +186,11 @@ int* almostSortedArray(int size)
     return array;
 }
 
+/**
+ * Function generating an array sorted in reverse order (descending)
+ * @param size The size of the array
+ * @return Generated array
+ */
 int* reverseArray(int size)
 {
     int* array = new int[size];
@@ -159,6 +204,11 @@ int* reverseArray(int size)
 
 //*********************************************************//
 
+/**
+ * Function changes the two elements in places
+ * @param element1 One element
+ * @param element2 The other element
+ */
 void swap(int& element1, int& element2)
 {
     int temp = element1;
@@ -166,6 +216,14 @@ void swap(int& element1, int& element2)
     element2 = temp;
 }
 
+/**
+ * Function performing binary search
+ * @param arr An array which is searched
+ * @param begin Initial position
+ * @param end End position
+ * @param target The item you need to find
+ * @return The element found in the array
+ */
 int binarySearch(int *arr, int begin, int end, int target)
 {
     while (begin - end < -1) {
@@ -189,6 +247,11 @@ int binarySearch(int *arr, int begin, int end, int target)
 
 //*********************************************************//
 
+/**
+ * Bubble sort
+ * @param arr The array to be sorted
+ * @param size The size of the array
+ */
 void bubbleSort(int *arr, int size)
 {
     for (int i = size; i > 0; --i) {
@@ -200,6 +263,11 @@ void bubbleSort(int *arr, int size)
     }
 }
 
+/**
+ * Bubble sort with Iverson Condition I
+ * @param arr The array to be sorted
+ * @param size The size of the array
+ */
 void bubbleSortIversonCondition1(int *arr, int size)
 {
     bool already_sorted = true;
@@ -217,6 +285,11 @@ void bubbleSortIversonCondition1(int *arr, int size)
     }
 }
 
+/**
+ * Bubble sort with Iverson Condition II
+ * @param arr The array to be sorted
+ * @param size The size of the array
+ */
 void bubbleSortIversonCondition2(int *arr, int size)
 {
     int last_swap = size - 1;
@@ -236,6 +309,11 @@ void bubbleSortIversonCondition2(int *arr, int size)
     } while (last_swap != 0);
 }
 
+/**
+ * Insertion sort
+ * @param arr The array to be sorted
+ * @param size The size of the array
+ */
 void insertionSort(int *arr, int size)
 {
     for (int i = 1; i < size; ++i) {
@@ -245,6 +323,11 @@ void insertionSort(int *arr, int size)
     }
 }
 
+/**
+ * Insertion Binary sort
+ * @param arr The array to be sorted
+ * @param size The size of the array
+ */
 void insertionBinarySort(int *arr, int size)
 {
     for (int i = 1; i < size; ++i) {
@@ -260,6 +343,11 @@ void insertionBinarySort(int *arr, int size)
     }
 }
 
+/**
+ * Counting sort
+ * @param arr The array to be sorted
+ * @param size The size of the array
+ */
 void countingSort(int *arr, int size)
 {
     int index = 0;
